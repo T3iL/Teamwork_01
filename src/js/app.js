aa68894bb629646905bbd3119b70415acc94c2c5
infrmtktl <infrmtktl@gmail.com>
2021-02-15 12:35:51 +0100

    query working

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,22 @@ import wypisz from './wypisz.js'
 
 const main = document.querySelector("#main")
 const summary = document.querySelector("#summary")
+const btn = document.getElementById("lupa")
+const input = document.querySelector("#query")
 
-querySelector().onclick = function(){myScript};
+// querySelector().onclick = function(){myScript};
 
+btn.addEventListener('click',zapytaj)
 
-function src(x){
-    let query = x
-    }
+function zapytaj(){
 
-fetch(`http://swapi.dev/api/people?search=${query}`)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-        wypisz(response)
-    })
+    const query = input.value;
+
+    console.log('wypisanie')
+    fetch(`http://swapi.dev/api/people?search=${query}`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            wypisz(response)
+        })
+}
